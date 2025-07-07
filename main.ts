df6177f7e572e42a69df446338182f07f5fdf713
Thiago Sam <thiagosamthi@gmail.com>
2025-07-07 14:13:24 -0400

    TypeScript
    Exercicio para criar funcoes tipadas em typescript.

---
+++ main.ts
@@ -0,0 +1,10 @@
+function multiplicacao(valorA: number, valorB: number): number {
+    return valorA * valorB;
+}
+
+function saudacao(nome: string): string {
+    return "Oi " + nome;
+}
+
+console.log(multiplicacao(10,20));
+console.log(saudacao('Thiago'));
